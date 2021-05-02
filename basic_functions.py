3ab9e185500424aed465eaf580a8e0da0e2ea603
Amit Manchanda <manchanda.amit@gmail.com>
2021-05-02 19:16:26 +0530

    Commit Basic Functions File

---
+++ basic_functions.py
@@ -0,0 +1,11 @@
+def add_nums(name):
+    print("HELLO " + name)
+    number_1 = input()
+    number_2 = input()
+    sum = int(number_1) + int(number_2)
+    return sum
+    #print("XYZ")
+
+
+#add_nums("AMITM")
+print(add_nums("AmitM"))
